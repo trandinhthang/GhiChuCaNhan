c316626da852d851fb3ad02dbabdc53db25921f7
caongocthien <caongocthien36@gmail.com>
2020-12-31 16:10:33 +0700

    del

--- app/src/main/java/com/example/personal_note/MainActivity.java
+++ app/src/main/java/com/example/personal_note/MainActivity.java
@@ -8,33 +8,12 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-    DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        drawerLayout = findViewById(R.id.drawerlayout);
     }
 
-    public void MenuClick(View view){
-        openDrawer(drawerLayout);
 
-    }
-
-    private static void openDrawer(DrawerLayout drawerLayout) {
-
-        drawerLayout.openDrawer(GravityCompat.START);
-
-    }
-
-    public void ClickLogo(View view){
-        closeDrawer(drawerLayout);
-    }
-
-    private void closeDrawer(DrawerLayout drawerLayout) {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
-            drawerLayout.closeDrawer(GravityCompat.START);
-        }
-    }
 }
